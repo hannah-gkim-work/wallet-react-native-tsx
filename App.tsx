f256cb6e81a97a47d5444dec81d8776f1bc9b882
Hannah Gomez Kim <hannah.gomezkim@slalom.com>
2022-02-03 16:29:49 -0600

    working on navigator

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 //custom font
 import AppLoading from "expo-app-loading";
-// import { useFonts } from "expo-font";
 import {
   useFonts,
   Lato_700Bold,
   Lato_400Regular,
 } from "@expo-google-fonts/lato";
+//React Navigator
+import RootStack from "./navigators/RootStack";
 import Welcome from "./screens/Welcome";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    // "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
-    // "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
     Lato_700Bold,
     Lato_400Regular,
   });
@@ -20,5 +18,5 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-  return <Welcome />;
+  return <RootStack />;
 }
