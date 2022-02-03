c5fceb542e5cfedbbd3f303f48a71ba3e34b915a
Hannah Gomez Kim <hannah.gomezkim@slalom.com>
2022-02-03 10:23:57 -0600

    working on regular button

---
+++ components/Buttons/RegularButton.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+//components
+import { colors } from "../colors";
+import RegularText from "../Texts/RegularText";
+
+const RegularButton = () => {
+  return <div></div>;
+};
+
+export default RegularButton;
+
+const ButtonView = styled.TouchableOpacity``;
